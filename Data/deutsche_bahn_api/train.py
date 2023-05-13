93e2320ca91f78dddacfa8c1efb43148c7e589ac
HassanRady <hassan.khaled.rady@gmail.com>
2023-05-13 09:52:38 +0200

    creating an info method for trains

--- Data/deutsche_bahn_api/train.py
+++ Data/deutsche_bahn_api/train.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import pandas as pd
 
 from deutsche_bahn_api.train_changes import TrainChanges
 
@@ -17,3 +18,15 @@ class Train:
     train_changes: TrainChanges | None
 
 
+    def info(self) -> pd.DataFrame:
+        df = pd.DataFrame({
+            "Train Number": self.train_number,
+            "Train Type": self.train_type,
+            "Platform": self.platform,
+            # "Arrival Time": self.arrival,
+            # "Train Line": self.train_line,
+            "Departure": self.departure,
+            "Planned Stations": self.stations,
+            "Stop ID": self.stop_id,
+            }, index=[0])
+        return df
