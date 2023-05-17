201c04c542f2b11cd2d44f3ac9f505c95ad2c893
HassanRady <hassan.khaled.rady@gmail.com>
2023-05-17 15:17:13 +0200

    making the data processor class static

--- Data/deutsche_bahn_api/data_processor.py
+++ Data/deutsche_bahn_api/data_processor.py
@@ -5,7 +5,8 @@ class DataProcessor:
     def __init__(self) -> None:
         pass
 
-    def process_date_format(self, date):
+    @staticmethod
+    def process_date_format(date):
         """Convert string date format (YYMMDDHHMM) to (YYYY-MM-DD-HH-MM).
 
         Args:
@@ -14,6 +15,13 @@ class DataProcessor:
         Returns:
             str: date in (YYYY-MM-DD-HH-MM) format.
         """
-        if date:
-            date = datetime.strptime(date, "%Y-%M-%D-%H-%M")
+        if date and not (date == "N/A") :
+            date = datetime.strptime(date, "%y%m%d%H%M").strftime('%Y-%m-%d %H:%M')
         return date
+    
+
+
+if __name__ == "__main__":
+    dp = DataProcessor()
+    out = dp.process_date_format("2305160945")
+    print(out)
