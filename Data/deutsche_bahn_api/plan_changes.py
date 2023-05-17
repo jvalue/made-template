2582d88e8b81ec470c175573dbfc278901269257
HassanRady <hassan.khaled.rady@gmail.com>
2023-05-17 11:44:22 +0200

    info method & setting default values

--- Data/deutsche_bahn_api/plan_changes.py
+++ Data/deutsche_bahn_api/plan_changes.py
@@ -1,25 +1,38 @@
 from deutsche_bahn_api.message import Message
+import pandas as pd
 
 
 class PlanChange:
     """ This class represents changed train attributes. """
-    EVA_NR: int
-    stop_id: int
-    next_stations: str
-    passed_stations: str
-    arrival: str
-    departure: str
-    platform: str
-    messages: list[Message]
 
+    def __init__(self) -> None:
+        self.station_number = None
+        self.stop_id = None
+        self.next_stations = None
+        self.passed_stations = None
+        self.arrival = None
+        self.departure = None
+        self.platform = None
 
     def insert_into_db(self, db_engine, table_name):
         db_engine.execute(
             f"""
             INSERT INTO {table_name} VALUES (
-                {self.EVA_NR}, '{self.stop_id}', '{self.next_stations}', '{self.passed_stations}', 
+                {self.station_number}, '{self.stop_id}', '{self.next_stations}', '{self.passed_stations}', 
                 '{self.arrival}', '{self.departure}', '{self.platform}'
               )
             """
         )
+        db_engine.commit()
     
+    def info(self) -> pd.DataFrame:
+        df = pd.DataFrame({
+            "Station Number": self.station_number,
+            "Stop ID": self.stop_id,
+            "Passed Stations": self.passed_stations,
+            "Next Stations": self.next_stations,
+            "Arrival Time": self.arrival,
+            "Departure Time": self.departure,
+            "Platform": self.platform,
+        }, index=[0])
+        return df
