dda1b4af399ed65f49c9843b1f20aa1919ee276f
HassanRady <hassan.khaled.rady@gmail.com>
2023-05-19 12:00:00 +0200

    exercise 1 implementation

---
+++ exercises/init_airports_data.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS airports (
+    column_1 int,
+    column_2 text,
+    column_3 text,
+    column_4 text,
+    column_5 text,
+    column_6 text,
+    column_7 float,
+    column_8 float,
+    column_9 int,
+    column_10 float,
+    column_11 text,
+    column_12 text,
+    geo_punkt float,
+    PRIMARY KEY (column_1)
+)
