1482865158e81736b529cb42f3a4ed0419db25bc
diganto-deb <digantodebbarma@gmail.com>
2023-05-20 16:24:46 +0200

    Resolved csv reading error

--- exercises/excercise1.py
+++ exercises/excercise1.py
@@ -12,7 +12,7 @@ column_types = [BIGINT, TEXT, TEXT, TEXT, TEXT, TEXT, FLOAT, FLOAT, FLOAT, TEXT]
 
 def build_data_pipeline(source_url, db_file, table_name, column_names, column_types):
     # Fetch the data from the source URL into a pandas DataFrame
-    df = pd.read_csv(source_url, delimiter = ';', error_bad_lines=False)
+    df = pd.read_csv(source_url, delimiter = ';')
     
     # Create the SQLite engine and connect to the database
     engine = create_engine(f"sqlite:///{db_file}")
