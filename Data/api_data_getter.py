bcfd907f5932e52634b21305caffa269f7686738
HassanRady <hassan.khaled.rady@gmail.com>
2023-05-30 11:54:54 +0200

    api data getter & inserter into database for airflow dag

--- Data/api_data_getter.py
+++ Data/api_data_getter.py
@@ -1,14 +1,12 @@
+from deutsche_bahn_api.api_caller import ApiClient
+from database_client import SqliteClient
+from deutsche_bahn_api.timetable_retrieval import TimeTableHandler
+from deutsche_bahn_api.station_loader import StationLoader
 import os
 from os.path import join
 from dotenv import load_dotenv
 dotenv_path = join(os.path.abspath(''), '.env')
 load_dotenv(dotenv_path)
-
-from deutsche_bahn_api.station_loader import StationLoader
-from deutsche_bahn_api.timetable_retrieval import TimeTableHandler
-
-from database_client import SqliteClient
-from deutsche_bahn_api.api_caller import ApiClient
 
 
 api_client = ApiClient(os.environ["DB_CLIENT_ID"], os.environ["DB_API_KEY"])
@@ -22,24 +20,28 @@ station_helper.load_stations()
 
 
 # TODO: tmp
-sample = 3
-
-for station in station_helper.stations_list[:sample]:
-    response = api_client.get_current_hour_station_timetable(station.EVA_NR)
-    if response.status_code != 200:
-        continue
-    else:
-        trains_in_this_hour = timetable_handler.get_timetable_data(response)
-        for train_plan in trains_in_this_hour:
-            train_plan.insert_into_db(SqliteClient.db_engine, "train_plan")
+sample = 100
 
 
-for station in station_helper.stations_list[:sample]:
-    response = api_client.get_all_timetable_changes_from_station(station.EVA_NR)
-    if response.status_code != 200:
-        continue
-    plans_change = timetable_handler.get_timetable_changes_data(response)
-    for plan_change in plans_change:
-        plan_change.insert_into_db(SqliteClient.db_engine, "plan_change")
+def get_data_to_database():
+    for station in station_helper.stations_list[:sample]:
+        response = api_client.get_current_hour_station_timetable(
+            station.EVA_NR)
+        if response.status_code != 200:
+            continue
+        else:
+            trains_in_this_hour = timetable_handler.get_timetable_data(
+                response)
+            for train_plan in trains_in_this_hour:
+                train_plan.insert_into_db(SqliteClient.db_engine, "train_plan")
 
-SqliteClient.db_engine.close()
+    for station in station_helper.stations_list[:sample]:
+        response = api_client.get_all_timetable_changes_from_station(
+            station.EVA_NR)
+        if response.status_code != 200:
+            continue
+        plans_change = timetable_handler.get_timetable_changes_data(response)
+        for plan_change in plans_change:
+            plan_change.insert_into_db(SqliteClient.db_engine, "plan_change")
+
+    SqliteClient.db_engine.close()
