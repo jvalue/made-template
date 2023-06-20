93d6a843fc516edef629007a5621f5a6842e6449
diganto-deb <digantodebbarma@gmail.com>
2023-06-20 20:45:22 +0200

    Validating columns

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -30,7 +30,8 @@ class CarDataPipeline:
         self.data['CIN'] = self.data['CIN'].astype(str).str.zfill(5)
         numeric_columns = ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
         self.data[numeric_columns] = self.data[numeric_columns].apply(pd.to_numeric, errors='coerce')
-        self.data = self.data[(self.data[numeric_columns] > 0)
+        self.data = self.data[(self.data[numeric_columns] > 0)]
+        
     def create_database(self):
         
         conn = sqlite3.connect(self.database)
