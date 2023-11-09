8557c517998f84070cfa8b95ddce91abbb007d3c
Muhammad Fahad Ali <mfahad.ali@fau.de>
2023-11-09 02:13:13 +0100

    correct comments

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,23 +1,22 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-# Define the URL of the data source
+# URL of the data source
 data_source_url = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
 
-# Define the SQLite database file and table name
+# SQLite database file and table name
 database_name = "airports.sqlite"
 table_name = "airports"
 
-# Download and Read the CSV data into a Pandas DataFrame
-    
+# Download and Read the CSV data into a Pandas DataFrame 
 try:
     data_frame = pd.read_csv(data_source_url, low_memory=False , sep=';')
 
-    # Create an SQLite engine and write the DataFrame to the database
+    # Create SQLite engine and write the DataFrame to the database
     engine = create_engine(f"sqlite:///{database_name}")
     data_frame.to_sql(table_name, engine, if_exists="replace", index=False)
 
-    # Close the database connection
+    # Close database connection
     engine.dispose()
 
     print(f"Data has been successfully written to {database_name} in the table {table_name}")
