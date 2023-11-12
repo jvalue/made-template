22881d3a852086dd680c4e6b82e220381f2a4a38
luca-dot-sh <->
2023-11-12 19:58:03 +0100

    fix filename in ex1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -5,6 +5,6 @@ import sqlalchemy as sqla
 url = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
 airports = pd.read_csv(url, sep=";")
 airports.set_index("column_1", inplace=True)
-engine = sqla.create_engine("sqlite:///airports.db")
+engine = sqla.create_engine("sqlite:///airports.sqlite")
 airports.to_sql("airports", engine, if_exists="replace", index=True)
 engine.dispose()
