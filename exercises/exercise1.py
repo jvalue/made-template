949a8fba7406c04bf65406006cc723a43e5d8fb1
luca-dot-sh <->
2023-11-12 19:48:02 +0100

    exercise1

---
+++ exercises/exercise1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import sqlalchemy as sqla
+
+
+url = "https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv"
+airports = pd.read_csv(url, sep=";")
+airports.set_index("column_1", inplace=True)
+engine = sqla.create_engine("sqlite:///airports.db")
+airports.to_sql("airports", engine, if_exists="replace", index=True)
+engine.dispose()
