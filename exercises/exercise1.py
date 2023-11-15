7f0408b43680cb88eabe07cdfe02fd2b864e6f05
vi29hero <148459043+vi29hero@users.noreply.github.com>
2023-11-15 13:21:24 +0100

    exercise1.py

---
+++ exercises/exercise1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sqlalchemy import String, TEXT, INTEGER, Float, DECIMAL
+
+data = pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv',sep=";", on_bad_lines='skip')
+col = {'column_1': INTEGER, 'column_2': String, 'column_3': String, 'column_4': String, 'column_5': String,
+               'column_6': String, 'column_7': Float,'column_8': Float, 'column_9': INTEGER, 'column_10': Float,
+               'column_11': TEXT,'column_12': String, 'geo_punkt': DECIMAL}
+
+data.to_sql('airports','sqlite:///airports.sqlite', if_exists='replace', index=False)
