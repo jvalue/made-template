83bdeb1a864a13051164747e37e6aa823f0a4c28
Impana Somashekar <148458215+yt75exud@users.noreply.github.com>
2023-11-15 13:17:42 +0100

    exercise1.py

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -2,10 +2,10 @@ import pandas as pd
 from sqlalchemy import String, TEXT, INTEGER, Float, DECIMAL
 
 #read the data
-df = pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit')
+df = pd.read_csv('https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv',sep=";", on_bad_lines='skip')
 
 #change the column type
-columnTypes = {'column_1': INTEGER, 'column_2': String, 'column_3': String, 'column_4': String, 'column_5': String,
+colTypes = {'column_1': INTEGER, 'column_2': String, 'column_3': String, 'column_4': String, 'column_5': String,
                'column_6': String, 'column_7': Float,'column_8': Float, 'column_9': INTEGER, 'column_10': Float,
                'column_11':TEXT,'column_12': String, 'geo_punkt': DECIMAL}
 
