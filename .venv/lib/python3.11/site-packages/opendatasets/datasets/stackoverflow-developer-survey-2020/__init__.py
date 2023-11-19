28eab62e3cd0408ce7d7fa396a4ef148d57c7ced
Arni Islam <arniislam@Arnis-MacBook-Air.local>
2023-11-19 23:41:31 +0600

    updated git ignore

--- .venv/lib/python3.11/site-packages/opendatasets/datasets/stackoverflow-developer-survey-2020/__init__.py
+++
@@ -1,9 +0,0 @@
-from opendatasets.utils import download_raw_files
-
-
-def download(dataset_id, data_dir, dry_run, **kwargs):
-    download_raw_files(dataset_id, data_dir, dry_run, [
-        'survey_results_public.csv',
-        'survey_results_schema.csv',
-        'README.txt'
-    ])
