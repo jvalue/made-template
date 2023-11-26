7c9844656fc62d9bb4aa9196d4b7cade9246e54b
Akshat khara <kharaakshat191@gmail.com>
2023-11-26 14:28:59 +0100

    Updated exercise2

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from io import StringIO
 import urllib.request
 import re
+
 # 1 : Downloading the CSV file
 url = 'https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV'
 response = urllib.request.urlopen(url)
@@ -16,18 +17,18 @@ df = df.drop("Status", axis='columns')
 valid_verkehr_values = ["FV", "RV", "nur DPN"]
 df = df[df['Verkehr'].isin(valid_verkehr_values)]
 
+# Filter "Laenge" and "Breite" columns based on the valid coordinate range
+# valid_coordinate_range = int(-90, 90)
+# valid_coordinate_range = int(valid_coordinate_range)
+
 numeric_columns = ['Laenge', 'Breite']
 df[numeric_columns] = df[numeric_columns].replace({',': '.'}, regex=True)
 df[['Laenge', 'Breite']] = df[['Laenge', 'Breite']].apply(pd.to_numeric)
 
 for x in df.index:
-    if df.loc[x, "Laenge"] <= -90:
-       df = df.drop(x, inplace=True)
-    elif df.loc[x, "Laenge"] >= 90:
+    if df.loc[x, "Laenge"] <= -90 or df.loc[x, "Laenge"] >= 90 :
         df = df.drop(x, inplace=True)
-    elif df.loc[x, "Breite"] <= -90:
-        df = df.drop(x, inplace=True)
-    elif df.loc[x, "Breite"] >= 90:
+    elif df.loc[x, "Breite"] <= -90 or df.loc[x, "Breite"] >= 90 :
         df = df.drop(x, inplace=True)
 
 
@@ -39,7 +40,6 @@ def is_valid_ifopt(value):
 
 df = df[df['IFOPT'].apply(is_valid_ifopt)]
 df = df.dropna()  # Drop rows with empty cells
-
 
 #  3: Defining the SQLite database connection and create an SQLAlchemy engine
 db_connection_str = 'sqlite:///trainstops.sqlite'
