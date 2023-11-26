d33a0d2cd99d9ac570a872622e85ec583fc5c16d
Akshat khara <kharaakshat191@gmail.com>
2023-11-26 15:09:50 +0100

    Updated script exercise2

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -4,21 +4,20 @@ from io import StringIO
 import urllib.request
 import re
 
-# 1 : Downloading the CSV file
+# Downloading and Reading the CSV file into a pandas DataFrame
 url = 'https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV'
 response = urllib.request.urlopen(url)
 csv_content = response.read().decode('utf-8')
-
-#  2: Reading the CSV file into a pandas DataFrame
 df = pd.read_csv(StringIO(csv_content),sep=';')
 df = df.drop("Status", axis='columns')
 
 # Filter "Verkehr" column
-valid_verkehr_values = ["FV", "RV", "nur DPN"]
-df = df[df['Verkehr'].isin(valid_verkehr_values)]
+Verkehr_valid_values = ["FV", "RV", "nur DPN"]
+df = df[df['Verkehr'].isin(Verkehr_valid_values)]
 
-numeric_columns = ['Laenge', 'Breite']
-df[numeric_columns] = df[numeric_columns].replace({',': '.'}, regex=True)
+# Filter for speicific dimension i.e. Latitude and Longtitude
+Dimension = ['Laenge', 'Breite']
+df[Dimension] = df[Dimension].replace({',': '.'}, regex=True)
 df[['Laenge', 'Breite']] = df[['Laenge', 'Breite']].apply(pd.to_numeric)
 
 for x in df.index:
@@ -29,20 +28,21 @@ for x in df.index:
 
 # Function to check the validity of IFOPT values
 def is_valid_ifopt(value):
-    # Define the pattern for valid IFOPT values
     pattern = r'^[a-zA-Z]{2}:\d+:\d+(?::\d+)?$'
     return pd.notna(value) and bool(re.match(pattern, str(value)))
-
 df = df[df['IFOPT'].apply(is_valid_ifopt)]
-df = df.dropna()  # Drop rows with empty cells
 
+# Drop cells with empty rows
+df = df.dropna()
+
+# Use fitting SQLite types (e.g., BIGINT, TEXT or FLOAT) for all columns
 df['Betreiber_Nr'] =  df['Betreiber_Nr'].astype(int)
 
-#  3: Defining the SQLite database connection and create an SQLAlchemy engine
+# Defining the SQLite database connection and create an SQLAlchemy engine
 db_connection_str = 'sqlite:///trainstops.sqlite'
 engine = create_engine(db_connection_str)
 
-#  4: Writing the DataFrame to the SQLite database
+# Writing the DataFrame to the SQLite database
 df.to_sql('trainstops', con=engine, index=False, if_exists='replace')
 
 # Close the engine
