d22996f7eb52541b524b47f544bceabba4b09a82
Akshat khara <kharaakshat191@gmail.com>
2023-11-26 14:31:12 +0100

    Updated script exercise2

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -31,7 +31,6 @@ for x in df.index:
     elif df.loc[x, "Breite"] <= -90 or df.loc[x, "Breite"] >= 90 :
         df = df.drop(x, inplace=True)
 
-
 # Function to check the validity of IFOPT values
 def is_valid_ifopt(value):
     # Define the pattern for valid IFOPT values
