b3bbbce8a63368d4a383fe0ac723908ca3acf0ef
Zeinab Aliakbari Mamaghani <148368514+zeinabaliakbari@users.noreply.github.com>
2023-12-05 16:19:14 +0100

    Update pipeline.sh

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+#!/bin/bash
+
+# Activate virtual environment (if applicable)
+# source /path/to/your/virtualenv/bin/activate
+
+# Run the Python script
 python project/pipeline.py
+
+# Deactivate virtual environment (if applicable)
+# deactivate
+
+
