70177e1f52e5eba6ba914cbedbcb602ed6057893
Ruchita Nathani <ruchita.nathani@fau.de>
2023-12-06 15:10:20 +0530

    Updated data collection file
    Signed-off-by: Ruchita Nathani <ruchita.nathani@fau.de>

--- project/data_collector.py
+++ project/data_collector.py
@@ -64,68 +64,12 @@ def download_dataset(kaggle_api):
     return
 
 def get_bitcoin_dataframe():
-    # kaggle_api = authenticate_kaggle()
-    # # Download the bitcoin dataset
-    # kaggle_api.dataset_download_files('varpit94/bitcoin-data-updated-till-26jun2021', path='.', unzip=True) 
-
-    # # Read the CSV file into a Pandas DataFrame
-    # df = pd.read_csv('BTC-USD.csv',sep=',')
-
-    # # created a SQLAlchemy engine to connect a bitcoin db
-    # data_path = './data/bitcoin.sqlite'
-    # engine = create_engine(f'sqlite:///{data_path}')
-
-    # types = {
-    #     'Date': NVARCHAR(length=128),
-    #     'Open': FLOAT(asdecimal=True),
-    #     'High': FLOAT(asdecimal=True),
-    #     'Low': FLOAT(asdecimal=True),
-    #     'Close': FLOAT(asdecimal=True),
-    #     'Adj': FLOAT(asdecimal=True),
-    #     'Close': FLOAT(asdecimal=True),
-    #     'Volume': BIGINT,
-    # }
-
-    # df.to_sql("bitcoin", con=engine, index=False, if_exists='replace', dtype=types)
+    
     bitcoin_df = pd.read_csv(DATASET_DICT["bitcoin"]["file_name"])
 
     return bitcoin_df
 
 def get_gold_price_dataframe():
-    # kaggle_api = authenticate_kaggle()
-    # # Download the gold-price dataset
-    # kaggle_api.dataset_download_files('odins0n/monthly-gold-prices', path='.', unzip=True) 
-
-    # # Read the CSV file into a Pandas DataFrame
-    # df = pd.read_csv('1990-2021.csv',sep=',')
-
-    # # created a SQLAlchemy engine to connect a gold-price db
-    # data_path = './data/gold-price.sqlite'
-    # engine = create_engine(f'sqlite:///{data_path}')
-
-    # types = {
-    #     'Date': NVARCHAR(length=128),
-    #     'United States(USD)': FLOAT(asdecimal=True),
-    #     'Europe(EUR)': FLOAT(asdecimal=True),
-    #     'Japan(JPY)': FLOAT(asdecimal=True),
-    #     'United Kingdom(GBP)': FLOAT(asdecimal=True),
-    #     'Canada(CAD)': FLOAT(asdecimal=True),
-    #     'Switzerland(CHF)': FLOAT(asdecimal=True),
-    #     'India(INR)': FLOAT(asdecimal=True),
-    #     'China(CNY)' : FLOAT(asdecimal=True),
-    #     'Turkey(TRY)' : FLOAT(asdecimal=True),
-    #     'Saudi Arabia(SAR)' : FLOAT(asdecimal=True),
-    #     'Indonesia(IDR)' : FLOAT(asdecimal=True),
-    #     'United Arab Emirates(AED)' : FLOAT(asdecimal=True),
-    #     'Thailand(THB)' : FLOAT(asdecimal=True),
-    #     'Vietnam(VND)' : FLOAT(asdecimal=True),
-    #     'Egypt(EGP)' : FLOAT(asdecimal=True),
-    #     'South Korean(KRW)' : FLOAT(asdecimal=True),
-    #     'Australia(AUD)' : FLOAT(asdecimal=True),
-    #     'South Africa(ZAR)' : FLOAT(asdecimal=True),
-    # }
-
-    # df.to_sql("gold-price", con=engine, index=False, if_exists='replace', dtype=types)
 
     gold_price_df = pd.read_csv(DATASET_DICT["gold-price"]["file_name"])
 
