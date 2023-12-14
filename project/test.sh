d2976990529496ee6c8d9217486b55ce61e01358
Muhammad Fahad Ali <mfahad.ali@fau.de>
2023-12-14 03:51:55 +0100

    update paths in test.sh file

--- project/test.sh
+++ project/test.sh
@@ -3,22 +3,19 @@
 
 echo "=== Executing pipeline ==="
 
-
-
+# Execute pipeline
+python project/pipeline.py
+  
 # Check dataset is avilable
 if [ -f "./datasets.sqlite" ]; then
   echo "Dataset exist."
-  
-
 else
   echo "Dataset not exist executing pipeline to fetchand create database."
-  
-  # Execute pipeline
-  python ./pipeline.py
-  
 fi
 
 echo "=== Running Tests ==="
-python test.py
+
+#Execute testcase
+python project/test.py
 
 read -p "Press any key to continue... " -n1 -s
