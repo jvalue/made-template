c63d72982798c0e3ff9f8108e0d6715883afa0a0
Muhammad Fahad Ali <mfahad.ali@fau.de>
2023-12-26 18:00:05 +0100

    update pipeline for reports graphs

--- project/pipeline.py
+++ project/pipeline.py
@@ -19,6 +19,18 @@ def data_extraction_xls(path):
     print("Finish: Data Extraction {} s ".format(t2 - t1))
     return df
 
+def data_extraction_csv(path):
+    t1 = time()
+    print("Data Extraction in progress...")
+
+    try:
+        df = pd.read_csv(path, encoding='latin-1')
+    except Exception as e:
+        print("Error occurred during file reading:", str(e))
+        return None
+    t2 = time()
+    print("Finish: Data Extraction {} s ".format(t2 - t1))
+    return df
 
 # Data Transformation 
 def data_transformation(data_frame, rename_col, drop_col):
@@ -59,34 +71,76 @@ def data_loader(db_file, data_frame, table_name):
 
 def main():
     
-    path_Immoscout24 = "https://docs.google.com/spreadsheets/d/1Bhi9CZjfI6qlM-2J1TVeDy_JfIcSBqufXlHV6IuT7n4/export?format=xlsx"
-    df1 = data_extraction_xls(path_Immoscout24)
-    df1_drop_cols = ["picturecount", "scoutId", "geo_bln", "geo_krs"]
-    df1_rename_cols = {
-        "regio1": "federalState",
-        "geo_plz": "zipCode",
-        "regio2": "district",
-        "regio3": "cityTown"
-    }
-    df1 = data_transformation(df1, df1_rename_cols, df1_drop_cols)
-    data_loader("datasets.sqlite", df1, "immoscout")
-
     path_intstudent = "https://docs.google.com/spreadsheets/d/1n9DXXIBUI5VpP8-qgCdhxvoV6FJgUyAVrz1VxAFZVNo/export?format=xlsx"
     df2 = data_extraction_xls(path_intstudent)
+
+    # correct date format in year cloumn
+    df2['Jahr'] = pd.to_datetime(df2['Jahr'].str.split('/').str[0], format='%Y')
+    df2['Jahr'] = df2['Jahr'].dt.strftime('%Y')
+
+    #drop rows with num values
+    df2 = df2.dropna()
+
+    # List of columns to convert from float64 to int
+    columns = ['Geisteswissenschaften', 'Sozialwissenschaften', 'Mathematik',
+                 'Ingenieurwissenschaften', 'Informatik',
+                 'medizin', 'Landwirtschaft']
+    
+    # Convert selected columns to int
+    df2[columns] = df2[columns].astype(int)
+    
     df2_drop_cols = []
     df2_rename_cols = {
-        "stadt": "city",
-        "universität": "university",
+        "Jahr": "Year",
+        "Stadt": "City",
+        "Universität": "University",
         "Geisteswissenschaften": "Humanities",
         "Sozialwissenschaften": "Social sciences",
         "Mathematik": "Mathematics",
         "Ingenieurwissenschaften": "Engineering sciences",
         "Informatik": "Computer Science",
-        "Medizin": "Medicine",
+        "medizin": "Medicine",
         "Landwirtschaft": "Agriculture"
     }
     df2 = data_transformation(df2, df2_rename_cols, df2_drop_cols)
     data_loader("datasets.sqlite", df2, "intstudents")
+
+    
+    #path_Immoscout24 = "https://docs.google.com/spreadsheets/d/1Bhi9CZjfI6qlM-2J1TVeDy_JfIcSBqufXlHV6IuT7n4/export?format=xlsx"
+    path_Immoscout24 = r"C:\Users\mual273859\Downloads\archive\immo_data.csv"
+    df1 = data_extraction_csv(path_Immoscout24)
+    
+    # Check if column is empty and drop corresponding rows
+    df1.dropna(subset=['regio1', 'regio2', 'noRooms'], inplace=True)
+    
+    # Fetch only Bayren state dataset
+    df1 = df1[df1['regio1'] == 'Bayern']
+    
+    # Drop not needed columns
+    df1_drop_cols = ["picturecount", "scoutId", "geo_bln", "geo_krs", "telekomHybridUploadSpeed", "telekomTvOffer", "newlyConst", "balcony", "picturecount", "pricetrend", "telekomUploadSpeed", "scoutId", "firingTypes", "yearConstructedRange", "interiorQual", "petsAllowed", "streetPlain", "lift", "baseRentRange", "typeOfFlat", "energyEfficiencyClass", "lastRefurbish", "electricityBasePrice", "electricityKwhPrice", "date"]
+    
+    # correct the formats of data values
+    df1['regio2'] = df1['regio2'].str.encode('latin-1').str.decode('utf-8')
+
+
+    df1_rename_cols = {
+        "regio1": "federalState",
+        "geo_plz": "zipCode",
+        "regio2": "City",
+        "regio3": "Town"
+    }
+
+    # # Change 'city' column value 'Deggendorf' to 'Deggendorf_kreis'
+    # df1.loc[df1['regio2'] == 'Deggendorf_kreis'] = 'Deggendorf'
+    
+    # only fetch data with city name available in students data
+    university_cities = df2['City'].unique()
+    df1 = df1[df1['regio2'].isin(university_cities)]
+
+    # Format datasets
+    df1 = data_transformation(df1, df1_rename_cols, df1_drop_cols)
+    data_loader("datasets.sqlite", df1, "immoscout")
+
 
 if __name__ == "__main__":
     main()
--- project/test.py
+++ project/test.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, inspect
-
+import pandas as pd
 from pipeline import (
     data_extraction_xls,
     data_transformation
@@ -7,6 +7,12 @@ from pipeline import (
 
 
 def test_dataset_extraction(path):
+    df = data_extraction_xls(path)
+    assert not df.empty, "Dataset Extraction Failed"
+    print("Extract Dataset: Test Passed")
+    return df
+
+def test_data_extraction_csv(path):
     df = data_extraction_xls(path)
     assert not df.empty, "Dataset Extraction Failed"
     print("Extract Dataset: Test Passed")
@@ -33,32 +39,36 @@ def test_dataset_loader(table_name):
 
 
 def test_pipeline():
-    path_Immoscout24 = "https://docs.google.com/spreadsheets/d/1Bhi9CZjfI6qlM-2J1TVeDy_JfIcSBqufXlHV6IuT7n4/export?format=xlsx"
-    df1 = test_dataset_extraction(path_Immoscout24)
-    df1_drop_cols = ["picturecount", "scoutId", "geo_bln", "geo_krs"]
-    df1_rename_cols = {
-        "regio1": "federalState",
-        "geo_plz": "zipCode",
-        "regio2": "district",
-        "regio3": "cityTown"
-    }
-
-    test_dataset_transformation(df1, df1_rename_cols, df1_drop_cols)
-
-    test_dataset_loader("immoscout")
 
     path_intstudent = "https://docs.google.com/spreadsheets/d/1n9DXXIBUI5VpP8-qgCdhxvoV6FJgUyAVrz1VxAFZVNo/export?format=xlsx"
     df2 = test_dataset_extraction(path_intstudent)
+
+    # correct date format in year cloumn
+    df2['Jahr'] = pd.to_datetime(df2['Jahr'].str.split('/').str[0], format='%Y')
+    df2['Jahr'] = df2['Jahr'].dt.strftime('%Y-%m-%d')
+
+    #drop rows with num values
+    df2 = df2.dropna()
+
+    # List of columns to convert from float64 to int
+    columns = ['Geisteswissenschaften', 'Sozialwissenschaften', 'Mathematik',
+                 'Ingenieurwissenschaften', 'Informatik',
+                 'medizin', 'Landwirtschaft']
+    
+    # Convert selected columns to int
+    df2[columns] = df2[columns].astype(int)
+
     df2_drop_cols = []
     df2_rename_cols = {
-        "stadt": "city",
-        "universität": "university",
+        "Jahr": "year",
+        "Stadt": "city",
+        "Universität": "university",
         "Geisteswissenschaften": "Humanities",
         "Sozialwissenschaften": "Social sciences",
         "Mathematik": "Mathematics",
         "Ingenieurwissenschaften": "Engineering sciences",
         "Informatik": "Computer Science",
-        "Medizin": "Medicine",
+        "medizin": "Medicine",
         "Landwirtschaft": "Agriculture"
     }
 
@@ -66,6 +76,40 @@ def test_pipeline():
 
     test_dataset_loader("intstudents")
 
+    #path_Immoscout24 = "https://docs.google.com/spreadsheets/d/1Bhi9CZjfI6qlM-2J1TVeDy_JfIcSBqufXlHV6IuT7n4/export?format=xlsx"
+    path_Immoscout24 = r"C:\Users\mual273859\Downloads\archive\immo_data.csv"
+    df1 = test_data_extraction_csv(path_Immoscout24)
+    
+    # Check if column is empty and drop corresponding rows
+    df1.dropna(subset=['regio1', 'regio2', 'noRooms'], inplace=True)
+    
+    # Fetch only Bayren state dataset
+    df1 = df1[df1['regio1'] == 'Bayern']
+    
+    # Drop not needed columns
+    df1_drop_cols = ["picturecount", "scoutId", "geo_bln", "geo_krs", "telekomHybridUploadSpeed", "telekomTvOffer", "newlyConst", "balcony", "picturecount", "pricetrend", "telekomUploadSpeed", "scoutId", "firingTypes", "yearConstructedRange", "interiorQual", "petsAllowed", "streetPlain", "lift", "baseRentRange", "typeOfFlat", "energyEfficiencyClass", "lastRefurbish", "electricityBasePrice", "electricityKwhPrice", "date"]
+    
+    # correct the formats of data values
+    df1['regio2'] = df1['regio2'].str.encode('latin-1').str.decode('utf-8')
+
+
+    df1_rename_cols = {
+        "regio1": "federalState",
+        "geo_plz": "zipCode",
+        "regio2": "City",
+        "regio3": "Town"
+    }
+
+    # Change 'city' column value 'Deggendorf' to 'Deggendorf_kreis'
+    df1.loc[df1['regio2'] == 'Deggendorf_kreis'] = 'Deggendorf'
+    
+    # only fetch data with city name available in students data
+    university_cities = df2['City'].unique()
+    df1 = df1[df1['regio2'].isin(university_cities)]
+
+    test_dataset_transformation(df1, df1_rename_cols, df1_drop_cols)
+
+    test_dataset_loader("immoscout")
 
 if __name__ == "__main__":
     test_pipeline()
