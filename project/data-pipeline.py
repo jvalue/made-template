ac3d58a5cb5eaffe7dfb5b88acafe59c13c798a6
Luca Preibsch <luca.preibsch@fau.de>
2024-05-20 19:34:28 +0200

    refactor emissions data pipeline and filter rows

--- project/data-pipeline.py
+++ project/data-pipeline.py
@@ -1,26 +1,56 @@
 import pandas as pd
+import sqlalchemy as sql
 
-destination_dir = '../data'
+destination_dir = 'data'
 
-print('running: data pipeline')
 
-# Datasource1: Net greenhouse gas emissions
-print('processing emissions csv')
-emissions_url = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/sdg_13_10/?format=SDMX-CSV&i'
-emissions_sheet = pd.read_csv(emissions_url, sep=',')
+def drop_rows(df, mask):
+    df.drop(df[mask].index, axis=0, inplace=True)
 
-emissions_sheet.to_sql('emissions', f'sqlite:///{destination_dir}/emissions.sqlite', if_exists='replace', index=False)
 
-# Datasource2: Primary energy consumption
-print('processing energy consumption csv')
-energy_consumption_url = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/sdg_07_10/?format=SDMX-CSV&i'
-energy_consumption_sheet = pd.read_csv(energy_consumption_url, sep=',')
-energy_consumption_sheet.to_sql('energy_consumption', f'sqlite:///{destination_dir}/energy_consumption.sqlite', if_exists='replace', index=False)
+def create_emissions_data(name, url, sql_engine):
+    print('downloading emissions data')
+    emissions_sheet = pd.read_csv(url, sep=',')
 
-# Datasource3: Share of energy from renewable sources
-print('processing energy share csv')
-energy_share_url = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/nrg_ind_ren/?format=SDMX-CSV&i'
-energy_share_sheet = pd.read_csv(energy_share_url, sep=',')
-energy_share_sheet.to_sql('energy_share', f'sqlite:///{destination_dir}/energy_share.sqlite', if_exists='replace', index=False)
+    print('processing emissions csv')
 
-print('success: data pipeline')
+    # filter rows
+    # 1. Source sectors for greenhouse gas emissions:
+    #    TOTXMEMONIA - Total (excluding memo items, including international aviation)
+    src_crf_mask = emissions_sheet['src_crf'] == 'TOTXMEMONIA'
+    drop_rows(emissions_sheet, src_crf_mask)
+
+    # 2. Unit of measure
+    #    T_HAB - Tonnes per capita
+    unit_mask = emissions_sheet['unit'] == 'T_HAB'
+    drop_rows(emissions_sheet, unit_mask)
+
+    # filter columns
+    emissions_sheet.drop(labels=['DATAFLOW', 'LAST UPDATE', 'freq', 'airpol', 'src_crf', 'unit', 'OBS_FLAG'], axis=1, inplace=True)
+
+    print('writing emissions data')
+    emissions_sheet.to_sql(name, sql_engine, if_exists='replace', index=False)
+
+
+if __name__ == '__main__':
+    sql_engine = sql.create_engine(f'sqlite:///{destination_dir}/data.sqlite')
+
+    print('running: data pipeline')
+
+    # Datasource1: Net greenhouse gas emissions
+    emissions_url = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/sdg_13_10/?format=SDMX-CSV&i'
+    create_emissions_data('emissions', emissions_url, sql_engine)
+
+    # Datasource2: Primary energy consumption
+    print('processing energy consumption csv')
+    energy_consumption_url = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/sdg_07_10/?format=SDMX-CSV&i'
+    energy_consumption_sheet = pd.read_csv(energy_consumption_url, sep=',')
+    energy_consumption_sheet.to_sql('energy_consumption', sql_engine, if_exists='replace', index=False)
+
+    # Datasource3: Share of energy from renewable sources
+    print('processing energy share csv')
+    energy_share_url = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/nrg_ind_ren/?format=SDMX-CSV&i'
+    energy_share_sheet = pd.read_csv(energy_share_url, sep=',')
+    energy_share_sheet.to_sql('energy_share', sql_engine, if_exists='replace', index=False)
+
+    print('success: data pipeline')
