029992c1d9a87f4a63029fca0e2c634748048e30
Mohammadreza <69771345+mmrzaa@users.noreply.github.com>
2024-05-23 11:30:19 +0000

    pipeline basic data cleaning

--- project/pipeline.py
+++ project/pipeline.py
@@ -1,7 +1,13 @@
 import pandas as pd
+import sqlite3 as db
+df = pd.read_csv(
+    "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/env_air_gge?format=SDMX-CSV&compressed=false",
+    sep= ",", header=0, na_filter=True)
 
-df = pd.read_csv( \
-    'https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/stadt-neuss-herbstpflanzung-2023/exports/csv', \
-    sep= ';'   )
+df.drop_duplicates(inplace = True)
 
-print (df.to_string())
+conn = db.connect("../data/dataset.db")
+
+df.to_sql("dataset", conn, if_exists="replace", index=False)
+
+conn.close()
