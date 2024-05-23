a50f6ce34f3b18ad4f2b84ceaed40a1ee2a856b0
mmrza <69771345+mmrzaa@users.noreply.github.com>
2024-05-23 11:14:58 +0200

    panda test

--- project/pipeline.py
+++ project/pipeline.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv( \
-    https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/stadt-neuss-herbstpflanzung-2023/exports/csv, \
+    'https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/stadt-neuss-herbstpflanzung-2023/exports/csv', \
     sep= ';'   )
 
 print (df.to_string())
