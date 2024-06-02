488de08b6573d1d128e69cdecac1fe15293622a2
Hassan8725 <hassanahmed8725@gmail.com>
2024-06-02 12:44:35 +0200

    visualization module updated

--- project/data_exploration/visualization.py
+++ project/data_exploration/visualization.py
@@ -13,7 +13,7 @@ def plot_overall_average_temperature_change(df: pd.DataFrame) -> None:
     year_columns = df.columns[2:]
     avg_temp_change = df[year_columns].mean()
 
-    plt.figure(figsize=(14, 7))
+    plt.figure(figsize=(10, 6))
     avg_temp_change.plot()
     plt.title("Average Worldwide Surface Temperature Change Over Years")
     plt.xlabel("Year")
@@ -49,7 +49,7 @@ def plot_compare_countries_temperature_trend(
     year_columns = df.columns[2:]
 
     # Plotting
-    plt.figure(figsize=(14, 7))
+    plt.figure(figsize=(10, 6))
     plt.plot(year_columns, country1_data[year_columns].values.flatten(), label=country1)
     plt.plot(year_columns, country2_data[year_columns].values.flatten(), label=country2)
     plt.title(
@@ -63,24 +63,87 @@ def plot_compare_countries_temperature_trend(
     plt.show()
 
 
-def plot_average_temperature_change_map(merged: gpd.GeoDataFrame) -> None:
+def plot_average_value_map(
+    merged: gpd.GeoDataFrame, label_and_unit: str, column: str = "Average_Value"
+) -> None:
     """
-    Create a map visualization of all countries with the average change in temperature.
+    Create a map visualization of all countries
+    with the average value of a specified column.
 
     :param merged: gpd.GeoDataFrame
-        GeoDataFrame containing the merged world map and temperature change data.
+        GeoDataFrame containing the merged world map and data.
+    :param label_and_unit: str
+        A string containing the label and unit for the legend and title.
+    :param column: str, optional
+        The name of the column to plot, default is "Average_Value".
     """
+    label, unit = label_and_unit.split(" (")
+    unit = unit.rstrip(")")
+
     fig, ax = plt.subplots(1, 1, figsize=(15, 10))
     merged.plot(
-        column="Average_Temperature_Change",
+        column=column,
         ax=ax,
         legend=True,
         legend_kwds={
-            "label": "Average Temperature Change (°C)",
+            "label": f"{label} ({unit})",
             "orientation": "horizontal",
         },
         cmap="viridis",
         missing_kwds={"color": "lightgrey"},
     )
-    plt.title("Average Surface Temperature Change by Country")
+    plt.title(f"{label} by Country")
+    plt.show()
+
+
+def plot_column_over_years(
+    df: pd.DataFrame, date_col: str, value_col: str, label: str
+) -> None:
+    """
+    Plot the specified column aggregated by year.
+
+    :param df: pd.DataFrame
+        DataFrame containing the data.
+    :param date_col: str
+        The name of the column containing date information.
+    :param value_col: str
+        The name of the column containing the values to be plotted.
+    :param label: str
+        The label for the data to be used in the plot title and ylabel.
+    """
+
+    # Convert date column to datetime
+    df[date_col] = pd.to_datetime(df[date_col])
+
+    # Set date column as index
+    df.set_index(date_col, inplace=True)
+
+    # Resample by year and calculate the mean
+    yearly_data = df[value_col].resample("Y").mean()
+
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    yearly_data.plot()
+    plt.title(f"{label} Over Years")
+    plt.xlabel("Year")
+    plt.ylabel(f"Average {label}")
+    plt.grid(True)
+    plt.show()
+
+
+def plot_average_change(avg_change: pd.Series, label: str) -> None:
+    """
+    Plot the average change over the years.
+
+    :param avg_change: pd.Series
+        Series containing the average change for each year.
+    :param label: str
+        The label for the data to be used in the plot title and ylabel.
+    """
+    plt.figure(figsize=(10, 6))
+    avg_change.plot()
+    plt.title(f"Average Worldwide {label} Change Over Years")
+    plt.xlabel("Year")
+    plt.ylabel(f"Average {label} Change")
+    plt.grid(True)
     plt.show()
