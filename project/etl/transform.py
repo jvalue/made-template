7c0651b995586f26a6435f0e29dbc61904a7d442
Muhammad Ali <62547802+muhammadalyy14@users.noreply.github.com>
2024-06-08 02:46:16 +0200

    Update transform.py

--- project/etl/transform.py
+++ project/etl/transform.py
@@ -22,6 +22,67 @@ def simplify_indicator(df):
     df['Indicator'] = df['Indicator'].apply(lambda x: x.split(':')[-1].strip())
     return df
 
+def standardize_country_names(df):
+    """
+    Standardize country names based on a predefined mapping.
+    Args:
+        df (pd.DataFrame): DataFrame with the 'Country' column to be standardized.
+    Returns:
+        pd.DataFrame: DataFrame with standardized country names.
+    """
+    country_mapping = {
+        "Afghanistan, Islamic Rep. of": "Afghanistan",
+        "Armenia, Rep. of": "Armenia",
+        "Azerbaijan, Rep. of": "Azerbaijan",
+        "Bahamas, The": "Bahamas",
+        "Belarus, Rep. of": "Belarus",
+        "Bosnia and Herzegovina": "Bosnia & Herzegovina",
+        "Cabo Verde": "Cape Verde",
+        "Central African Rep.": "Central African Republic",
+        "China, P.R.: Hong Kong": "Hong Kong",
+        "China, P.R.: Macao": "Macao",
+        "China, P.R.: Mainland": "China",
+        "Congo, Dem. Rep. of the": "Democratic Republic of the Congo",
+        "Congo, Rep. of": "Republic of the Congo",
+        "Côte d'Ivoire": "Ivory Coast",
+        "Czech Rep.": "Czech Republic",
+        "Dominican Rep.": "Dominican Republic",
+        "Egypt, Arab Rep. of": "Egypt",
+        "Eswatini, Kingdom of": "Eswatini",
+        "Ethiopia, The Federal Dem. Rep. of": "Ethiopia",
+        "Fiji, Rep. of": "Fiji",
+        "Gambia, The": "Gambia",
+        "Iran, Islamic Rep. of": "Iran",
+        "Kazakhstan, Rep. of": "Kazakhstan",
+        "Korea, Dem. People's Rep. of": "North Korea",
+        "Korea, Rep. of": "South Korea",
+        "Kyrgyz Rep.": "Kyrgyzstan",
+        "Lao People's Dem. Rep.": "Laos",
+        "Macedonia, Republic of": "North Macedonia",
+        "Madagascar, Rep. of": "Madagascar",
+        "Micronesia, Federated States of": "Micronesia",
+        "Moldova, Rep. of": "Moldova",
+        "Myanmar": "Myanmar (Burma)",
+        "Russian Federation": "Russia",
+        "São Tomé and Príncipe, Dem. Rep. of": "São Tomé and Príncipe",
+        "Serbia, Rep. of": "Serbia",
+        "Slovak Rep.": "Slovakia",
+        "Tanzania, United Rep. of": "Tanzania",
+        "United Arab Emirates": "UAE",
+        "United Kingdom": "UK",
+        "United States": "USA",
+        "Yemen, Rep. of": "Yemen"
+    }
+    
+    non_existent_countries = [
+        "Germany Dem Rep (former)", "Germany Fed Rep (former)",
+        "Netherlands Antilles", "Soviet Union (former)"
+        
+    ]
+    df['Country'] = df['Country'].replace(country_mapping)
+    df = df[~df['Country'].isin(non_existent_countries)]
+    return df
+
 def apply_transformations(df, table_name):
     """
     Apply transformations to the DataFrame based on the table name.
@@ -43,5 +104,6 @@ def apply_transformations(df, table_name):
         relevant_columns = [col for col in column_mappings[table_name] if col in df.columns]
         df = df[relevant_columns]
         df = rename_year_columns(df)
+        df = standardize_country_names(df)
 
     return df
