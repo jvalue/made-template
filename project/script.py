3a8a192e3de5691c4d44d42c190388319544b13d
segalanastasiia <anastasiia.n.segal@gmail.com>
2024-06-12 10:10:39 +0200

    automated testing updates

--- project/script.py
+++ project/script.py
@@ -4,64 +4,87 @@ from sqlalchemy import create_engine
 
 ####################################################
 # 1st dataset: Healthy life years at birth
-df1 = pd.read_csv( \
-    'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/tps00150/?format=SDMX-CSV&lang=en&label=label_only', \
-    sep=','
-)
+def extract_hly():
+    url = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/tps00150/?format=SDMX-CSV&lang=en&label=label_only'
+    df1 = pd.read_csv(url, sep=',')
+    
+    return df1
 
-#look at the data to see what is in the dataframe
-print(df1)
 
-# exclude geneder data, as I do not use it in my analysis
-df1 = df1[~df1['sex'].isin(['Females', 'Males'])]
+def transform_hly(df1):
+    # exclude gender data, as I do not use it in my analysis
+    df1 = df1[~df1['sex'].isin(['Females', 'Males'])].copy()
 
-# drop unnesessary columns
-df1.drop(columns=['DATAFLOW','LAST UPDATE', 'freq', 'unit', 'indic_he', 'OBS_FLAG', 'sex'], inplace=True)
+    # drop unnecessary columns
+    df1.drop(columns=['DATAFLOW','LAST UPDATE', 'freq', 'unit', 'indic_he', 'OBS_FLAG', 'sex'], inplace=True)
 
-# turn years into columns
-df1_new = df1.pivot(index='geo', columns='TIME_PERIOD')['OBS_VALUE']
-df1 = df1_new
-df1.reset_index(inplace=True)
+    # turn years into columns
+    df1_new = df1.pivot(index='geo', columns='TIME_PERIOD')['OBS_VALUE']
+    df1 = df1_new
+    df1.reset_index(inplace=True)
 
-# fill in missing values by interpolating starting from the second column (skip "geo" columns of string type)
-df1.iloc[:, 1:] = df1.iloc[:, 1:].interpolate(method='linear', axis=1) # interpolate over country dimension (rows)
+    # fill in missing values by interpolating starting from the second column (skip "geo" columns of string type)
+    df1.iloc[:, 1:] = df1.iloc[:, 1:].interpolate(method='linear', axis=1) # interpolate over country dimension (rows)
 
-# fill in the missing values for the first year with the next year's value
-df1.iloc[:, 1:] = df1.iloc[:, 1:].bfill(axis=1)
+    # fill in the missing values for the first year with the next year's value
+    df1.iloc[:, 1:] = df1.iloc[:, 1:].bfill(axis=1)
+    
+    return df1
 
-# specify the directory where to load the dataset
-engine1 = create_engine('sqlite:///../data/dataset_hly.sqlite')
 
-# load the table into a sink (sqlite file)
-df1.to_sql('dataset_hly', engine1, if_exists='replace', index=False)
+def load_hly(df1, path):
+    # specify the directory where to load the dataset
+    engine_hly = create_engine(f'sqlite:///{path}')
+
+    # load the table into a sink (sqlite file)
+    df1.to_sql('dataset_hly', engine_hly, if_exists='replace', index=False)
 
 
 
 
 ####################################################
 # 2nd dataset: net greenhouse gas emissions per capita
-df2 = pd.read_csv( \
-    'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/sdg_13_10/?format=SDMX-CSV&lang=en&label=label_only', \
-    sep=','
-)
+def extract_gasem():
+    url = 'https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/sdg_13_10/?format=SDMX-CSV&lang=en&label=label_only'
+    df2 = pd.read_csv(url, sep=',')
+    
+    return df2
 
-#look at the data to see what is in the dataframe
-print(df2)
 
-# exclude index and other Total data, as I do not use it in my analysis (i use only tonnes per capita)
-df2 = df2[~df2['unit'].isin(['Index, 1990=100'])]
-df2 = df2[~df2['src_crf'].isin(['Total (excluding LULUCF and memo items, including international aviation)'])]
+def transform_gasem(df2):
+    # exclude index and other Total data, as I do not use it in my analysis (i use only tonnes per capita)
+    df2 = df2[~df2['unit'].isin(['Index, 1990=100'])].copy()
+    df2 = df2[~df2['src_crf'].isin(['Total (excluding LULUCF and memo items, including international aviation)'])].copy()
 
-# drop unnecessary columns
-df2.drop(columns=['DATAFLOW','LAST UPDATE', 'freq', 'airpol', 'unit', 'src_crf', 'OBS_FLAG'], inplace=True)
+    # drop unnecessary columns
+    df2.drop(columns=['DATAFLOW','LAST UPDATE', 'freq', 'airpol', 'unit', 'src_crf', 'OBS_FLAG'], inplace=True)
 
-# turn years into columns
-df2_new = df2.pivot(index='geo', columns='TIME_PERIOD')['OBS_VALUE']
-df2 = df2_new
-df2.reset_index(inplace=True)
+    # turn years into columns
+    df2_new = df2.pivot(index='geo', columns='TIME_PERIOD')['OBS_VALUE']
+    df2 = df2_new
+    df2.reset_index(inplace=True)
+    
+    return df2
 
-# specify the directory where to load the dataset
-engine2 = create_engine('sqlite:///../data/dataset_gasem.sqlite')
 
-# load the table into a sink (sqlite file)
-df2.to_sql('dataset_gasem', engine2, if_exists='replace', index=False)
+def load_gasem(df2, path):
+    # specify the directory where to load the dataset
+    engine_gasem = create_engine(f'sqlite:///{path}')
+
+    # load the table into a sink (sqlite file)
+    df2.to_sql('dataset_gasem', engine_gasem, if_exists='replace', index=False)
+
+
+def pipeline():
+    df1 = extract_hly()
+    df1 = transform_hly(df1)  
+    load_hly(df1, '../data/dataset_hly.sqlite')
+
+    df2 = extract_gasem()
+    df2 = transform_gasem(df2)
+    load_gasem(df2, '../data/dataset_gasem.sqlite')
+
+
+if __name__ == '__main__':
+    pipeline()
+    
---
+++ project/tests.py
@@ -0,0 +1,101 @@
+from numpy import NaN
+from script import transform_hly, load_hly, transform_gasem, load_gasem
+
+import pandas as pd
+import sqlite3
+from pandas.testing import assert_frame_equal
+from sqlalchemy import create_engine
+
+
+##############################################
+# tests for the 1st dataset
+def test_transform_hly():
+    data = pd.DataFrame({
+        'geo': ['Place1', 'Place1', 'Place1', 'Place2', 'Place2', 'Place2'],
+        'TIME_PERIOD': [2011, 2012, 2013, 2011, 2012, 2013],
+        'OBS_VALUE': [1.0, 2, NaN, 4.0, NaN, 6],
+        'sex': ['male', 'female', 'total', 'male', 'female', 'total'],
+        'DATAFLOW': ['a', 'b', 'c', 'd', 'e', 'f'],
+        'LAST UPDATE': ['2024', '2024', '2024', '2024', '2024', '2024'],
+        'freq': ['y', 'y', 'y', 'y', 'y', 'y'],
+        'unit': ['hly', 'hly', 'hly', 'hly', 'hly', 'hly'],
+        'indic_he': ['a', 'b', 'c', 'd', 'e', 'f'],
+        'OBS_FLAG': ['a', 'b', 'c', 'd', 'e', 'f']
+    })
+    # data.shape = (6, 10)
+    # data.isnull().any()
+    
+    transformed_data = transform_hly(data)
+    
+    # expected transformed_data.shape = (2, 4)
+    # expected transformed_data.isnull().any().any() = False
+    assert transformed_data.shape == (2, 4)
+    assert transformed_data.isnull().any().any() == False
+    print('passed test_transform_hly')
+
+   
+def test_load_hly():
+    data = pd.DataFrame({
+        'geo': ['x', 'y', 'z'],
+        '2012': [5, NaN, 6],
+        '2013': [3, 4, 5]
+    })
+    
+    test_path = '../project/test_load_hly.sqlite'
+    load_hly(data, test_path)
+    
+    conn = sqlite3.connect(test_path)
+    result = pd.read_sql_query("SELECT * FROM dataset_hly", conn)
+    conn.close()
+    
+    assert_frame_equal(result, data)
+    print('passed test_load_hly')
+    
+
+##############################################
+# tests for the 2nd dataset
+def test_transform_gasem():
+    data = pd.DataFrame({
+        'geo': ['Place1', 'Place1', 'Place1', 'Place2', 'Place2', 'Place2'],
+        'TIME_PERIOD': [2011, 2012, 2013, 2011, 2012, 2013],
+        'OBS_VALUE': [1.0, 2, NaN, 4.0, NaN, 6],
+        'DATAFLOW': ['a', 'b', 'c', 'd', 'e', 'f'],
+        'LAST UPDATE': ['2024', '2024', '2024', '2024', '2024', '2024'],
+        'freq': ['y', 'y', 'y', 'y', 'y', 'y'],
+        'airpol': ['a', 'b', 'c', 'd', 'e', 'f'],
+        'src_crf': ['a', 'b', 'c', 'd', 'e', 'f'],
+        'unit': ['tonnes', 'tonnes', 'tonnes', 'tonnes', 'tonnes', 'tonnes'],
+        'OBS_FLAG': ['a', 'b', 'c', 'd', 'e', 'f']
+    })
+    # data.shape = (6, 10)
+      
+    transformed_data = transform_gasem(data)
+    
+    # expected transformed_data.shape = (2, 4)    
+    assert transformed_data.shape == (2, 4)
+    print('passed test_transform_gasem')
+
+   
+def test_load_gasem():
+    data = pd.DataFrame({
+        'geo': ['a', 'b', 'c'],
+        '2012': [8, NaN, 9],
+        '2013': [2, 4.0, 7]
+    })
+    
+    test_path = '../project/test_load_gasem.sqlite'
+    load_gasem(data, test_path)
+    
+    conn = sqlite3.connect(test_path)
+    result = pd.read_sql_query("SELECT * FROM dataset_gasem", conn)
+    conn.close()
+    
+    assert_frame_equal(result, data)
+    print('passed test_load_gasem')
+
+
+if __name__ == '__main__':
+    test_transform_hly()
+    test_load_hly()
+    test_transform_gasem()
+    test_load_gasem()
