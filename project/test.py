96d64eef22e6815154f89b795c6b07a9f6d18338
Shubham Gupta <infotoshubham@gmail.com>
2024-06-14 19:54:23 +0200

    updated test case; made test cases more robust; improved code coverage

--- project/test.py
+++ project/test.py
@@ -1,6 +1,10 @@
 import sqlite3
+
+import pandas
 import pytest
 import os
+import requests
+# from pytest_mock import MockerFixture
 
 from ETL_Pipeline import ETL_Cpi
 
@@ -24,11 +28,146 @@ def create_dbConnection():
     finally:
         conn.close()
 
-def test_is_table_present(create_dbCursor):
+
+# test case for kaggle dataset availability
+def test_is_kaggle_link_available():
+    response = requests.get(
+        "https://www.kaggle.com/datasets/mdazizulkabirlovlu/all-countries-temperature-statistics-1970-2021/data?select=all+countries+global+temperature.csv")
+    assert response.status_code == 200
+
+
+# checks for extractions
+def test_data_extraction():
+    pipeline = ETL_Cpi()
+    result = pipeline.extraction()
+    assert isinstance(result, tuple)
+    assert len(result) == 3
+    assert isinstance(result[0], pandas.DataFrame)
+    assert isinstance(result[1], pandas.DataFrame)
+    assert isinstance(result[2], list)
+
+
+# check for crop data extraction
+def test_crop_data_extraction():
+    pipeline = ETL_Cpi()
+    result = pipeline.extract_data_crop_prd()
+    assert isinstance(result, list)
+    assert len(result) == 5
+
+
+# checks for transformations
+def test_data_transformation():
+    pipeline = ETL_Cpi()
+    result = pipeline.transformation()
+    assert isinstance(result, tuple)
+    assert len(result) == 3
+    assert isinstance(result[0], pandas.DataFrame)
+    assert isinstance(result[1], pandas.DataFrame)
+    assert isinstance(result[2], pandas.DataFrame)
+
+
+# test for number of items in crop database should be 23
+def test_num_of_crop_categories(create_dbCursor):
+    create_dbCursor.execute("SELECT DISTINCT Item from crop_data")
+
+
+# check for columns in all data frames
+def test_column_names_in_data():
+    pipeline = ETL_Cpi()
+    result = pipeline.transformation()
+    actual_columns_cpi_data = ['country_name', '1960', '1961', '1962', '1963', '1964', '1965', '1966',
+                               '1967', '1968', '1969', '1970', '1971', '1972', '1973', '1974', '1975',
+                               '1976', '1977', '1978', '1979', '1980', '1981', '1982', '1983', '1984',
+                               '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992', '1993',
+                               '1994', '1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002',
+                               '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2011', '2012',
+                               '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021',
+                               '2022']
+    actual_columns_in_crop_data = ['country_name', 'Unit', 'Change ', '1970', '1971', '1972', '1973',
+                                   '1974', '1975', '1976', '1977', '1978', '1979', '1980', '1981', '1982',
+                                   '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991',
+                                   '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000',
+                                   '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009',
+                                   '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018',
+                                   '2019']
+    actual_columns_in_temp_data = ['index', 'country_name', 'Item', 'Element', 'Unit', 'Y1970', 'Y1971',
+                                   'Y1972', 'Y1973', 'Y1974', 'Y1975', 'Y1976', 'Y1977', 'Y1978', 'Y1979',
+                                   'Y1980', 'Y1981', 'Y1982', 'Y1983', 'Y1984', 'Y1985', 'Y1986', 'Y1987',
+                                   'Y1988', 'Y1989', 'Y1990', 'Y1991', 'Y1992', 'Y1993', 'Y1994', 'Y1995',
+                                   'Y1996', 'Y1997', 'Y1998', 'Y1999', 'Y2000', 'Y2001', 'Y2002', 'Y2003',
+                                   'Y2004', 'Y2005', 'Y2006', 'Y2007', 'Y2008', 'Y2009', 'Y2010', 'Y2011',
+                                   'Y2012', 'Y2013', 'Y2014', 'Y2015', 'Y2016', 'Y2017', 'Y2018', 'Y2019']
+
+    actual_column_list = [actual_columns_cpi_data, actual_columns_in_crop_data, actual_columns_in_temp_data]
+    i = 0
+    for df in result:
+        expected_columns = result[i].columns.tolist()
+        assert expected_columns == actual_column_list[i]
+        i += 1
+
+
+# check for data types of columns
+def test_data_types_of_cpi_data():
+    pipeline = ETL_Cpi()
+    
+    actual_data_type_cpi_data = ['string', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64']
+    actual_data_type_crop_data = [ 'string', 'string', 'string', 'float64', 'float64', 'float64',
+       'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+       'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+       'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+       'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+       'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+       'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+       'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+       'float64', 'float64', 'float64', 'float64', 'float64']
+    actual_data_type_temp_data = [ 'int64', 'string', 'string', 'string', 'string', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64', 'float64', 'float64', 'float64', 'float64', 'float64',
+     'float64']
+
+    actual_data_type = [actual_data_type_cpi_data, actual_data_type_crop_data, actual_data_type_temp_data]
+
+    result = pipeline.transformation()
+    i = 0
+    for df in result:
+        expected_columns\
+            = list(df.dtypes)
+        expected_columns = [str(i) for i in expected_columns]
+        assert expected_columns == actual_data_type[i]
+        i += 1
+
+
+def test_is_table_cpi_present(create_dbCursor):
     create_dbCursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     all_table_list = create_dbCursor.fetchall()
     assert ('cpi_data',) in all_table_list
+
+
+def test_is_table_temp_present(create_dbCursor):
+    create_dbCursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    all_table_list = create_dbCursor.fetchall()
     assert ("temp_data",) in all_table_list
+
+
+def test_is_table_crop_present(create_dbCursor):
+    create_dbCursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    all_table_list = create_dbCursor.fetchall()
     assert ('crop_data',) in all_table_list
 
 
@@ -40,66 +179,3 @@ def test_is_pipeline_running(create_dbConnection):
     db_tables_count = cursor.fetchall()
     assert len(db_tables_count) > 0, ("The database does not contain any tables, hence it is not a valid SQLite "
                                       "database.")
-
-# def extract_column_info(columns):
-#     return [(name, type_) for _, name, type_, _, _, _ in columns]
-#
-#
-# @pytest.mark.parametrize("table_name, expected_columns", [
-#     # ("food_price_inflation", [
-#     #     ("Open", "FLOAT"),
-#     #     ("High", "FLOAT"),
-#     #     ("Low", "FLOAT"),
-#     #     ("Close", "FLOAT"),
-#     #     ("Inflation", "FLOAT"),
-#     #     ("country", "TEXT"),
-#     #     ("ISO3", "TEXT"),
-#     #     ("date", "TEXT"),
-#     # ]),
-#     ("temperature", [
-#         ("Area Code", "BIGINT"),
-#         ("Area Code (M49)", "TEXT"),
-#         ("Area", "TEXT"),
-#         ("Months Code", "BIGINT"),
-#         ("Months", "TEXT"),
-#         ("Element", "TEXT"),
-#         ("Unit", "TEXT"),
-#     ])
-# ])
-# def test_table_columns(create_dbCursor, table_name, expected_columns):
-#     create_dbCursor.execute(f"PRAGMA table_info({table_name});")
-#     columns = create_dbCursor.fetchall()
-#     extracted_columns = extract_column_info(columns)
-#     for column, data_type in expected_columns:
-#         assert (column, data_type) in extracted_columns
-#
-#
-# @pytest.mark.parametrize("table_name", ["temperature"  # , "food_price_inflation"
-#                                         ])
-# def test_table_data(create_dbCursor, table_name):
-#     create_dbCursor.execute(f"SELECT COUNT(*) FROM {table_name};")
-#     count = create_dbCursor.fetchone()[0]
-#     assert count > 0
-#
-#
-# @pytest.mark.parametrize("table_name, column_name, min_value, max_value", [
-#     ("temperature", "Y1961", -50, 50),
-#     # ("food_price_inflation", "Inflation", -100, 1000)
-# ])
-# def test_data_validity(create_dbCursor, table_name, column_name, min_value, max_value):
-#     query = f"SELECT {column_name} FROM {table_name} WHERE {column_name} NOT BETWEEN ? AND ?;"
-#     create_dbCursor.execute(query, (min_value, max_value))
-#     invalid_values = create_dbCursor.fetchall()
-#     assert len(invalid_values) == 0, f"Found invalid values in column {column_name} of table {table_name}"
-#
-#
-# @pytest.mark.parametrize("table_name, year_column_prefix, start_year, end_year", [
-#     ("temperature", "Y", 1961, 2020),
-#     # ("food_price_inflation", "Y", 2000, 2020)
-# ])
-# def test_time_series_consistency(create_dbCursor, table_name, year_column_prefix, start_year, end_year):
-#     for year in range(start_year, end_year + 1):
-#         column_name = f"{year_column_prefix}{year}"
-#         create_dbCursor.execute(f"SELECT COUNT(*) FROM {table_name} WHERE {column_name} IS NULL;")
-#         null_count = create_dbCursor.fetchone()[0]
-#         assert null_count == 0, f"Found NULL values in column {column_name} of table {table_name}"
