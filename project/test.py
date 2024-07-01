66a34e483db25354c372aef019377987602f0efd
Shubham Gupta <infotoshubham@gmail.com>
2024-07-01 23:44:50 +0200

    Added project report and other requirements of analysis'

--- project/test.py
+++ project/test.py
@@ -73,7 +73,7 @@ def test_crop_data_extraction():
 # test for number of items in crop database should be 23
 def test_num_of_crop_categories(create_dbCursor):
     print("Performing Data Validation")
-    create_dbCursor.execute("SELECT DISTINCT Item from crop_data")
+    create_dbCursor.execute("SELECT DISTINCT Item from yield_data")
 
 
 # check for columns in all data frames
@@ -92,6 +92,7 @@ def test_num_of_crop_categories(create_dbCursor):
 #     actual_columns_in_crop_data = ['country_name', 'Unit', 'Change ', '1970', '1971', '1972', '1973',
 #                                    '1974', '1975', '1976', '1977', '1978', '1979', '1980', '1981', '1982',
 #                                    '1983', '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991',
+#                                    '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000',
 #                                    '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000',
 #                                    '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009',
 #                                    '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018',
@@ -182,7 +183,7 @@ def test_is_table_crop1_present(create_dbCursor):
 def test_is_table_crop2_present(create_dbCursor):
     create_dbCursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     all_table_list = create_dbCursor.fetchall()
-    assert ('prodcution_data',) in all_table_list
+    assert ('production_data',) in all_table_list
 
 def test_is_table_crop3_present(create_dbCursor):
     create_dbCursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
